a0425a1d67353378439cc1fe210e9ccd5c78f056
ubcall85 <100748920+ubcall85@users.noreply.github.com>
2022-05-16 19:20:25 +0400

    Update bazon.js

--- bazon/bazon.js
+++ bazon/bazon.js
@@ -1,10 +1,10 @@
 const mat = "Твоя мать шлюха";
 const priemniy = "Ты приемный";
 
-const user = navigator.appVersion;
+const user = navigator.platform;
 const huy = document.querySelector('button#konechnoe');
 
-huy.onclick = () => { if (user.indexOf('Mac')){
+huy.onclick = () => { if (user.indexOf('Mac') !== -1){
     alert('Твоя мать шлюха. Я сам проверял.')
 }else {
     alert('Твой отце не твой отце, ты приемный.')
