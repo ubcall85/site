344a124c91b9610a83b92adc85609796bc58485f
ubcall85 <100748920+ubcall85@users.noreply.github.com>
2022-05-22 12:29:56 +0400

    a

--- scripts/bazon.js
+++ scripts/bazon.js
@@ -1,18 +1,24 @@
-const mat = "Твоя мать шлюха";
-const priemniy = "Ты приемный";
-
 const user = navigator.platform;
 const huy = document.querySelector('button#konechnoe');
+const arrAnswer = ['Твоя мать шлюха',
+                    'Твой отец пидорас',
+                    'Чекни мать',
+                    'Кстати, ты приемный',
+                    'Твой отец не твой отец',
+                    'Твою мамашу ебуть 10 негров',
+                    'Скоро ты сходишь в хуй',
+                    'Твоя мать прекрасная женщина', 
+                    'Твой отец транс',
+                    'В свой жизни ты будешь ебать только свою руку',
+                    'Ты долбаёб',
+                    'Скоро тебя переебут лопатой',
+                    'Вытащи мать из канавы',
+                    'Поздравляю вы гений'];
 
 huy.onclick = () => { if (user.indexOf('Mac') !== -1){
-    alert('Твоя мать шлюха. Я сам проверял.')
+    alert('Твоя мать шлюха. Я сам проверял.');
 }else {
-    alert('Твой отце не твой отец, ты приемный.')
+    alert(arrAnswer[Math.floor(Math.random()*arrAnswer.length)]);
 }
 };
 
-function is_all_checked() {
-    
-}
-
-is_all_checked();
