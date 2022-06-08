464d9ba1dbb711210cb8b4ab565035fdeabf2238
ubcall85 <100748920+ubcall85@users.noreply.github.com>
2022-06-08 08:06:13 +0400

    ф

--- scripts/carti.js
+++ scripts/carti.js
@@ -1,12 +1,3 @@
-/*
- const thread_sleep = mseconds => new Promise( resolve => setTimeout( resolve, mseconds ) ); // Шаманский прикол, на бизнес встрече по асинхронным потокам объясню
- button.onclick = async function() { // async обязательно для активации асинхронных потоков
-  element_outside.style.transform = 'rotateY(90deg)';
-  await thread_sleep(1000); // Заставляет поток затормозить на 1 секунду (1000 милисекунд), await обязательно
-  element_inside.style.transform = 'rotate(180deg)'; // Изначально у него rotate(90deg);
-}
-*/
-
 const arrPredict = ['Твоя мамаша дура ебаная',
                     'Твоя мать шлюха',
                     'Твой отец пидорас',
@@ -23,83 +14,79 @@ const arrPredict = ['Твоя мамаша дура ебаная',
                     'Вытащи мать из канавы',
                     'Поздравляю вы гений'];
 const amount = document.querySelector('input#cifra');
-const a = amount.value;
 
-function getRandomCard (min, max) {  
-    return  Math.floor(Math.random() * (max - min + 1)) + min;
+let cards = []; // объявляем массив, это будет пул, откуда выдергиваются числа на рандом. дважды одно и то же число оттуда дернуть нельзя.
+let cards_proto = new Array( 78 ); // Здесь мы объявляем прототипный массив, который мы будем копировать в пул, чтобы из него можно было дергать числа.
+cards_proto.fill(0) // Здесь мы заполняем прото-массив нулями
+    
+cards_proto = Array.prototype.map.call( cards_proto, (_, i) => i ); // Здесь мы присваиваем каждой ячейке массива в значение ее индекс, типа нулевой элемент = 0, первый элемент = 1 и т.д
+
+function refresh_array() {
+    cards = [...cards_proto]; // Здесь мы копируем прото-массив в пул
 }
 
-function vizov() {
-    const IMG = document.createElement('img');
-    let source = `../assets/taro/${getRandomCard(1, 78)}.jpg`;
-    IMG.src = source;
-    document.querySelector('.vivodCart').appendChild(IMG);
-}   
+refresh_array() // Это ОБЯЗАТЕЛЬНО вызываешь в начале скрипта, чтобы массив заполнился возможными картами, и вызываешь когда в него нужно вернуть все карты, чтобы из них можно было выбирать
 
-function vizovButton(){
-    const br = document.createElement('br');
-    document.querySelector('.vivodCart').appendChild(br);
 
-    const buttonCheck = document.createElement("button");
-    buttonCheck.id = 'check';
+function unique_random() {
+    if ( cards.length < 1 ) return false; // Если в пуле нет чисел, значит все карты уже разложены
+    let idx = Math.floor(Math.random() * cards.length); // Выбираем случайный элемент из пула
 
-    buttonCheck.innerText = "Проверить";
-    buttonCheck.onclick = function(){
-        alert(arrPredict[Math.floor(Math.random()*arrPredict.length)]);
-    }
-
-    document.querySelector('.vivodCart').appendChild(buttonCheck);
-
-    const buttonClear = document.createElement("button");
-    buttonClear.id = 'clear';
-
-    buttonClear.innerText = "Заново";
-    buttonClear.onclick = function(){
-        const col = document.querySelectorAll('img');
-        for (let c of col) {
-            c.remove();
-        }
-        document.querySelector('input#cifra').value="";
-        document.querySelector('br').remove();
-        document.querySelectorAll('button').remove();
-        alert('Введите новую цифру');
-    }
-    document.querySelector('.vivodCart').appendChild(buttonClear);
-
+    return cards.splice(idx, 1)[0] + 1; // удаляем элемент из пула и выводим его в функции, прибавляем единицу ибо индекс с 0, а нам надо от 1 до 78
 }
+
+// unique_random() вернет одно из чисел от 1 до 78, но при этом оно не вернет повторяющееся число, пока ты не вызовешь refresh_array();
 
 function is_int(v){
     return v - 0 == v;
 }
 
-function yes(v){
-    if (v == 1, v == 2, v == 3, v == 4, v == 5){
-        return v;
-    } else {
-        alert('idi v ochko');
-    }
-
-}
-
-function vivod(){
-    if (is_int(amount.value) == false){
-        alert('ДУРА, ХУЛИ ТЫ БУКВЫ ПИШЕШЬ? ПИШИ ЦИФРЫ ОТ 1 ДО 5');
-    }else if(0 < amount.value > 5){
-        alert('ДУРА, ПИШИ ОТ 1 ДО 5');
-    }else if (yes(amount.value)){
-        for(let i = 0;amount.value != i;i++){vizov();
+function displayCard(){
+    refresh_array();
+    if (is_int(amount.value) == true && amount.value != 0 && amount.value < 6){
+        for(let i = 0; amount.value != i; i++){
+            const card = document.createElement('img');
+            let num = unique_random();
+            let source = `../assets/taro/${num}.jpg`;
+            card.src = source;
+            card.id = num;
+            document.querySelector('.cardPlace').appendChild(card);
         }
-        vizovButton();
+        displayButton();
+    } else{
+        alert('ДУРА, ПИЩИ ЦИФРЫ ОТ 1 ДО 5');
     }
+}
+
+function displayButton(){
+    const br = document.createElement('br');
+
+    const checkButton = document.createElement('button');
+    checkButton.id = 'checkButton';
+    checkButton.innerText = 'ПРОВЕРИТЬ';
     
-}
+    const clearButton = document.createElement('button');
+    clearButton.id = 'clearButton'
+    clearButton.innerText = 'ЗАНОВО';
 
-function clear(){
-    for(let i = 0;amount.value != i;i++){
-        document.querySelector('img').remove('img');
+    checkButton.onclick = function(){
+        alert(arrPredict[Math.floor(Math.random()*arrPredict.length)]);
     }
-}
 
-function prediction(){
+    clearButton.onclick = function(){
+        const col = document.querySelectorAll('img');
+        for(let c of col){
+            c.remove();
+        }
 
+        document.querySelector('input#cifra').value="";
+        document.querySelector('br').remove();
+        document.querySelector('button#checkButton').remove();
+        document.querySelector('button#clearButton').remove();
+        alert('ВВЕДИТЕ НОВОЕ ЗНАЧЕНИЕ');
+
+    }
+    document.querySelector('.cardPlace').appendChild(br);
+    document.querySelector('.cardPlace').appendChild(checkButton);
+    document.querySelector('.cardPlace').appendChild(clearButton);
 }
