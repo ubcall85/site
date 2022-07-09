ca25235f7da8176cc188dc537edd3eb54a4f290b
ubcall85 <100748920+ubcall85@users.noreply.github.com>
2022-07-09 15:21:15 +0400

    a

--- db/util.js
+++ db/util.js
@@ -1,28 +1,37 @@
-function ajax( dest, payload_obj ) {
-    return new Promise( function(resolve) {
-        let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if ( this.readyState == 4 ) {
-                if ( this.status == 200 ) {
-                    resolve( this.responseText );
-                }
-            }
-        }
+// function ajax( dest, payload_obj ) {
+//     return new Promise( function(resolve) {
+//         let xhr = new XMLHttpRequest();
+//         xhr.onreadystatechange = function() {
+//             if ( this.readyState == 4 ) {
+//                 if ( this.status == 200 ) {
+//                     resolve( this.responseText );
+//                 }
+//             }
+//         }
 
-        let keys = Object.keys( payload_obj );
-        let serialized_chunks = [];
-        for ( let key of keys ) {
-            if (!payload_obj[key]) { continue };
-            serialized_chunks.push( `${key}=${payload_obj[key]}` );
-        }
+//         let keys = Object.keys( payload_obj );
+//         let serialized_chunks = [];
+//         for ( let key of keys ) {
+//             if (!payload_obj[key]) { continue };
+//             serialized_chunks.push( `${key}=${payload_obj[key]}` );
+//         }
 
-        xhr.open( 'POST', dest, false );
-        xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
-        xhr.send( serialized_chunks.join('&') );
-    } );
+//         xhr.open( 'POST', dest, false );
+//         xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+//         xhr.send( serialized_chunks.join('&') );
+//     } );
+// }
+
+// async function get_all_in_category( cat ) {
+//     let data = await ajax( '/api/get.php', { category: cat } );
+//     return JSON.parse(data);
+// };
+
+let db_placeholder = [
+    { id: 4, name: "Ерёмкин Андрей Вячеславович", data: null, category: "students" },
+    { id: 1, name: "Крестелёв Анатолий Иванович", data: null, category: "teachers" }
+    ];
+
+function getAllInCategory ( cat ){
+
 }
-
-async function get_all_in_category( cat ) {
-    let data = await ajax( '/api/get.php', { category: cat } );
-    return JSON.parse(data);
-};
